fa804d8ce03713c21793305345cc796efac0887b
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-05 23:32:03 -0500

    update name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React January 2021 Examples
         </a>
       </header>
     </div>

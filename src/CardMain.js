82107c972e6975a38ff7aa0a61dab8030f9e0c72
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-07 10:28:07 -0500

    updated main card with input field

--- src/CardMain.js
+++ src/CardMain.js
@@ -5,10 +5,11 @@ import {useState} from 'react';
 function CardMain(props) {
 
   const [count, setCount] = useState(0);
-
+  const [firstName, setFirstName] = useState("");
   
   return (
     <div className="CardMain">
+      Name: <input onChange={e=> setFirstName(e.target.value)} />
       <p>This is a {props.productName}</p>
       <button className="button" onClick={() => setCount(count + 1)}>Clicked {count} times.</button>
     </div>

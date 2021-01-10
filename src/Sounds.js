1a9b0a6587debd580cc43296dc68d669b5944212
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-10 09:03:43 -0500

    modify sound module

--- src/Sounds.js
+++ src/Sounds.js
@@ -1,10 +1,17 @@
 import useSound from 'use-sound';
 import noise_riff from './media/colby - candy gun - this is all just a bunch of fucking noise - combined and stacked for bandcamp restrictions.mp3'
+import {useState} from 'react';
 
-const Sounds = () => {
+function Sounds() {
     const [play] = useSound(noise_riff);
-  
-    return <button onClick={play}>Boop!</button>;
-  };
+    const [playSound, setPlaySound] = useState(true);
+
+    return (
+        <div>
+            <input onChange={!playSound ? play : null} />
+            <button onClick={()=>setPlaySound(!playSound)}>State:</button>
+        </div>
+    );
+}
 
   export default Sounds;

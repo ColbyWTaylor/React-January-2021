5bd9aebfb50bf3be21c3003c4eda450842b11b0f
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-19 21:18:21 -0500

    card data extract

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -4,15 +4,29 @@ import CardMain from './CardMain';
 
 function Cards(props) {
 
+
+  let cardData = [
+    {productName: "Code Flow i/o Poster",
+    productInfo: "It is a poster"
+   },
+    {productName: "Code Flow Meditation Podcast",
+    productInfo: "Limited run audio podcast episodes"
+   },
+    {productName: "Apparel",
+    productInfo: "Look dashingly comfortable from the comfort of our clothes."
+   },
+    {productName: "Health Test",
+    productInfo: "A checklist for keeping it 100%"
+   },
+  ]
+
   const [count, setCount] = useState(0);
   const [firstName, setFirstName] = useState("");
   
   return (
     <>
-    <CardMain productName="Code Flow i/o Poster" productInfo="It is a poster"/>
-    <CardMain productName="Code Flow Meditation Podcast" productInfo="Limited run audio podcast episodes"/>
-    <CardMain productName="Apparel" productInfo="Look dashingly comfortable from the comfort of our clothes."/>
-    <CardMain productName="Health Test" productInfo="A checklist for keeping it 100%"/>
+    <h1>Cards</h1>
+      {cardData.map((element, index)  => <CardMain key={index} {...element} />)}
     </>
 
   );
